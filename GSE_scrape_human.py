445136c2dd31bbd1ff4e5a1cbbc9ff8b73d10ae6
HM Rando <hrando@smith.edu>
2024-01-16 11:21:01 -0500

    clean up code

--- GSE_scrape_human.py
+++ GSE_scrape_human.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python
 
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-import re
-import json
-import sys
-from tqdm import trange
-
-
 """GSE Accession Analysis for human: Wordcloud for GSM Characteristics
  by Xinran Bi
- 
+
  Modified by Halie Rando as follows:
 - Converted from GSE_scrape_human.ipynb on Jan 14 at 12:05pm using `jupyter nbconvert --to script`
 - Debugged return statement being outside of an if loop
 - Refactored to use the same code for both mouse and human
 """
+import re
+import json
+import sys
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
 def import_GSE(species):
     """Filters a dataframe form GREIN by species
@@ -25,11 +22,12 @@ def import_GSE(species):
     """
     GREIN_data = pd.read_csv("data/GREIN_data.csv")
     if species == "mouse":
-        GREIN = GREIN_data[GREIN_data.Species == 'Mus musculus']
-    elif species == "human":
-        GREIN = GREIN_data[GREIN_data.Species == 'Homo sapiens']
-    GSE = GREIN['GEO accession'].tolist()
-    return GSE
+        return GREIN_data[GREIN_data.Species == 'Mus musculus']['GEO accession'].tolist()
+    if species == "human":
+        return GREIN_data[GREIN_data.Species == 'Homo sapiens']['GEO accession'].tolist()
+    print("Error: Unknown species")
+    sys.exit(1)
+
 
 def scrape_geo_data(geo_id):
     url = "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc={0}".format(geo_id)
@@ -40,15 +38,15 @@ def scrape_geo_data(geo_id):
             soup = BeautifulSoup(response.text, 'html.parser')
 
             # Find all <a> tags with href attributes containing "GSM"
-            gsm_links = soup.find_all('a', href=lambda href: href and href.startswith('/geo/query/acc.cgi?acc=GSM'))
+            gsm_links = soup.find_all('a',
+                                      href=lambda href: href and href.startswith('/geo/query/acc.cgi?acc=GSM'))
 
             # Extract and store only the GSM values
             gsm_values = [link.text for link in gsm_links]
 
             return gsm_values
 
-        else:
-            return "Failed to retrieve the page. Status code: {0}".format(response.status_code)
+        return "Failed to retrieve the page. Status code: {0}".format(response.status_code)
 
     except requests.exceptions.RequestException as e:
         return "Error: {0}".format(e)
@@ -63,8 +61,6 @@ def scrape_characteristics(geo_id):
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, 'html.parser')
 
-            characteristics_dict = {}
-
             # Find the "Characteristics" label
             characteristics_label = soup.find('td', text='Characteristics')
 
@@ -75,12 +71,10 @@ def scrape_characteristics(geo_id):
 
                 return characteristics_string
 
-    
         return "Failed to retrieve the page. Status code: {0}".format(response.status_code)
 
     except requests.exceptions.RequestException as e:
         return "Error: {0}".format(e)
-
 
 def extract_characteristics(input_str):
     input_str = re.sub(r'<td[^>]*>', '', input_str)  # remove <td> tags
@@ -93,7 +87,6 @@ def save_results_to_file(results, filename):
     with open(filename, 'w', encoding='utf-8') as file:
         for result in results:
             file.write(result + '\n')
-
 
 # Define species from standard in
 species = str()
@@ -116,8 +109,8 @@ if GSMs:
 GSM2 = len(GSM)
 
 if GSM1 != GSM2:
-    print("There is a bug-- the first GSM list is {0} and the second is {1}".format(G2M1, GSM2))
-    exit(1)
+    print("There is a bug-- the first GSM list is {0} and the second is {1}".format(GSM1, GSM2))
+    sys.exit(1)
 
 results = {}
 for GSM in GSM:
@@ -131,4 +124,3 @@ with open(json_file, "w") as file:
     json.dump(results, file)
 
 print("Characteristics saved to {0}".format(json_file))
-
